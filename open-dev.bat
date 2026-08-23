dde2f3b56ba6770e5ce370629df9c0ec81216338
proteinbeer <proteinbeer@gmail.com>
2026-08-23 20:57:10 +0200

    Compact header search button, unstick header, expose dev server on LAN

--- open-dev.bat
+++ open-dev.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d C:\Users\Proteinbeer\Desktop\Vibe\Danmaek
-start "" cmd /k npm.cmd run dev
+start "" cmd /k npm.cmd run dev -- --host
 timeout /t 2 >nul
 start "" http://localhost:4321/
