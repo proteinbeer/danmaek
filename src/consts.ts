41189c6b0e3216dd8ae392912461996946d0e5d7
proteinbeer <proteinbeer@gmail.com>
2026-09-11 15:43:27 +0200

    Add Star Wars Zero Company to guide subcategories

--- src/consts.ts
+++ src/consts.ts
@@ -36,7 +36,8 @@ export const GUIDE_SUBCATEGORIES: Subcategory[] = [
   { name: '더 블러드 오브 던워커', slug: 'dawnwalker' },
   { name: '귀무자', slug: 'onimusha' },
   { name: '발하임', slug: 'valheim' },
-  { name: '워독스', slug: 'wardogs' }
+  { name: '워독스', slug: 'wardogs' },
+  { name: '스타워즈 제로 컴퍼니', slug: 'star-wars-zero-company' }
 ];
 
 export const COUPON_SUBCATEGORIES: Subcategory[] = [
